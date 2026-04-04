fe2792a09b507104f22d0f12e9c6b8453e146e15
FractureFX <leg0rmail@gmail.com>
2026-04-04 14:34:24 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Song Info in Pause
+This mod adds a button to the pause menu that opens the info page of the level's song.
+
+# Support
+If you encounter any bugs with the mod, please be sure to contact me on <cb>**Discord.**</c> (Username is @FractureFX).
---
+++ src/main.cpp
@@ -0,0 +1,56 @@
+#include <Geode/modify/PauseLayer.hpp>
+#include <Geode/modify/SongInfoLayer.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/ui/Notification.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MyPauseLayer, PauseLayer) {
+	void customSetup() {
+		PauseLayer::customSetup();
+		auto leftMenu = this->getChildByID("left-button-menu");
+		auto rightMenu = this->getChildByID("right-button-menu");
+		auto settingPos = Mod::get()->getSettingValue<std::string>("position");
+		auto targetMenu = (settingPos == "Left") ? leftMenu : rightMenu;
+
+		if (!targetMenu) {
+			targetMenu = leftMenu;
+			return;
+		}
+
+		auto spr = CCSprite::createWithSpriteFrameName("edit_eSongBtn_001.png");
+		spr->setScale(0.8f);
+		auto btn = CCMenuItemSpriteExtra::create(
+			spr, this, menu_selector(MyPauseLayer::onSongInfoButton)
+		);
+
+		targetMenu->addChild(btn, 0);
+		targetMenu->updateLayout();
+	}
+
+	void onSongInfoButton(CCObject* sender) {
+		auto pl = PlayLayer::get();
+		auto lvl = pl->m_level;
+		auto songID = lvl->m_songID;
+		auto mdm = MusicDownloadManager::sharedState();
+		auto songinfo = mdm->getSongInfoObject(songID);
+
+		if (!songinfo) {
+			Notification::create("No song info available!", NotificationIcon::Error)->show();
+			return;
+		}
+		auto ngUrl = "https://www.newgrounds.com/audio/listen/" + std::to_string(songinfo->m_songID);
+		auto fixedYTurl = "https://www.youtube.com/channel/" + songinfo->m_youtubeChannel;
+		SongInfoLayer::create(
+			songinfo->m_songName,
+			songinfo->m_artistName,
+			ngUrl,
+			ngUrl,
+			fixedYTurl, 
+			"",
+			songID,
+			"",
+			songinfo->m_nongType
+		)->show();
+	}
+};
